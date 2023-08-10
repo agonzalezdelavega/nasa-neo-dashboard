ca6465275fd5e4bb0d00f78d541b84626e057b2f
agonzalezdelavega <albertogdelavega@gmail.com>
2023-08-10 01:35:09 -0500

    Dashboard - largest NEO data

--- lambda/get-neo-data/index.py
+++ lambda/get-neo-data/index.py
@@ -4,57 +4,67 @@ import json
 import logging
 import sys
 from datetime import datetime as dt
+import pandas as pd
+
 
 # ---- LOGGING CONFIGURATION --- #
 
-logger = logging.getLogger("neo-get-data")
+logger = logging.getLogger('neo-get-data')
 logger.setLevel(logging.INFO)
 ch = logging.StreamHandler(stream=sys.stdout)
 ch.setLevel(logging.INFO)
-formatter = logging.Formatter("%(asctime)s %(levelname)s: %(message)s")
+formatter = logging.Formatter('%(asctime)s %(levelname)s: %(message)s')
 ch.setFormatter(formatter)
 logger.addHandler(ch)
 
 # ---- CONNECT TO SDK ---- #
 
-logger.info("Connecting to SDK")
+logger.info('Connecting to SDK')
 try:
-    dynamodb_client = boto3.client("dynamodb", region_name="us-east-2")
-    lambda_client = boto3.client("lambda", region_name="us-east-2")
+    dynamodb_client = boto3.client('dynamodb', region_name='us-east-2')
+    lambda_client = boto3.client('lambda', region_name='us-east-2')
 except:
-    logger.error("Could not connect to AWS SDK")
+    logger.error('Could not connect to AWS SDK')
 
 def handler(event, context):
 # ---- LOAD DATA FROM DYNAMODB ---- #
     # Use current date as default if no date argument is passed
-    event["date"] = dt.today().strftime("%Y-%m-%d") if event["date"] == "" else event["date"]
+    event['date'] = dt.today().strftime('%Y-%m-%d') if event['date'] == '' else event['date']
     
     # Extract date from DynamoDB table
-    logger.info(f"Loading data from DynamoDB table for date: {event['date']}")
-    neo_data = get_neo_data(date=event["date"])
+    logger.info(f'Loading data from DynamoDB table for date: {event["date"]}')
+    neo_data = get_neo_data(date=event['date'])
 
     # If not available, trigger update function and read data again    
-    if neo_data["Count"] == 0:
-        logger.info("Entry not found, updating NEOs table")
+    if neo_data['Count'] == 0:
+        logger.info('Entry not found, updating NEOs table')
         response = lambda_client.invoke(
-            FunctionName=os.environ.get("LAMBDA_FUNCTION_PUT"),
-            Payload=json.dumps({"date": event["date"]})
+            FunctionName=os.environ.get('LAMBDA_FUNCTION_PUT'),
+            Payload=json.dumps({'date': event['date']})
         )
-        neo_data = get_neo_data(date=event["date"])
+        neo_data = get_neo_data(date=event['date'])
             
+# ---- GET MAX VALUE FOR LARGEST ESTIMATED DIAMETER ---- #
+    df = pd.DataFrame(neo_data["Items"])
+    df['name'] = [df['name'][i]['S'] for i in range(0, df.shape[0])]
+    df['estimated_diameter_feet_max'] = [float(df['estimated_diameter_feet_max'][i]['N']) for i in range(0, df.shape[0])]
+    df['relative_velocity_mph'] = [float(df['relative_velocity_mph'][i]['N']) for i in range(0, df.shape[0])]
+    largest_neo = df[['name', 'estimated_diameter_feet_max', 'relative_velocity_mph']].iloc[[df['estimated_diameter_feet_max'].idxmax()]]
+    neo_data['largest_neo'] = largest_neo.to_dict(orient='split', index=False)
+    
     return neo_data
 
 def get_neo_data(date):
     response = dynamodb_client.query(
-        TableName=os.environ.get("DYNAMO_DB_TABLE_NAME"),
-        IndexName=os.environ.get("GLOBAL_SECONDARY_INDEX"),
-        Select="ALL_ATTRIBUTES",
+        TableName=os.environ.get('DYNAMO_DB_TABLE_NAME'),
+        IndexName=os.environ.get('GLOBAL_SECONDARY_INDEX'),
+        Select='ALL_ATTRIBUTES',
         ExpressionAttributeValues={
-            ":date": {
-                "S": date,
+            ':date': {
+                'S': date,
             }
         },
-        KeyConditionExpression="close_approach_date = :date",
+        KeyConditionExpression='close_approach_date = :date',
     )
     return response
     
--- lambda/put-neo-data/index.py
+++ lambda/put-neo-data/index.py
@@ -6,107 +6,108 @@ import os
 import time
 from datetime import datetime as dt
 
+
 # ---- LOGGING CONFIGURATION --- #
 
-logger = logging.getLogger("neo-put-data")
+logger = logging.getLogger('neo-put-data')
 logger.setLevel(logging.INFO)
 ch = logging.StreamHandler(stream=sys.stdout)
 ch.setLevel(logging.INFO)
-formatter = logging.Formatter("%(asctime)s %(levelname)s: %(message)s")
+formatter = logging.Formatter('%(asctime)s %(levelname)s: %(message)s')
 ch.setFormatter(formatter)
 logger.addHandler(ch)
 
 # ---- CONNECT TO SDK ---- #
 
-logger.info("Connecting to SDK")
+logger.info('Connecting to SDK')
 try:
-    api_client = boto3.client("apigatewaymanagementapi", region_name="us-east-2")
-    ssm_client = boto3.client("ssm", region_name="us-east-2")
-    dynamodb_client = boto3.client("dynamodb", region_name="us-east-2")
+    api_client = boto3.client('apigatewaymanagementapi', region_name='us-east-2')
+    ssm_client = boto3.client('ssm', region_name='us-east-2')
+    dynamodb_client = boto3.client('dynamodb', region_name='us-east-2')
 except:
-    logger.error("Could not connect to AWS SDK")
+    logger.error('Could not connect to AWS SDK')
 
 # ---- GET NASA API CONNECTION PARAMETERS ---- #
 
-logger.info("Retriving API key from SSM")
-API_KEY = ssm_client.get_parameter(Name="nasa-api-key")
-if API_KEY["ResponseMetadata"]["HTTPStatusCode"] == 200:
-    logger.info("API key received")
+logger.info('Retriving API key from SSM')
+API_KEY = ssm_client.get_parameter(Name='nasa-api-key')
+if API_KEY['ResponseMetadata']['HTTPStatusCode'] == 200:
+    logger.info('API key received')
 else:
-    logger.error("Error retrieving API key")
-URL = "https://api.nasa.gov/neo/rest/v1/feed?"
+    logger.error('Error retrieving API key')
+URL = 'https://api.nasa.gov/neo/rest/v1/feed?'
 
 
 def handler(event, context):
         
 # ---- RECEIVE AND PROCESS DATA FROM API ---- #
     # Use current date as default if no date argument is passed
-    event["date"] = dt.today().strftime("%Y-%m-%d") if event["date"] == "" else event["date"]
+    event['date'] = dt.today().strftime('%Y-%m-%d') if event['date'] == '' else event['date']
     
     params = {
-        "api_key": API_KEY["Parameter"]["Value"],
-        "start_date": event["date"],
-        "end_date": event["date"]
+        'api_key': API_KEY['Parameter']['Value'],
+        'start_date': event['date'],
+        'end_date': event['date']
     }
 
-    logger.info(f"Querying API for date: {event['date']}")
+    logger.info(f'Querying API for date: {event["date"]}')
     response = requests.get(url=URL, params=params)
     if response.status_code == 200:
-        logger.info("HTTP 200 response received, proceeding with database query")
+        logger.info('HTTP 200 response received, proceeding with database query')
     else:
-        logger.error(f"API returned {response.status_code} error for requested date: {event['date']}")
+        logger.error(f'API returned {response.status_code} error for requested date: {event["date"]}')
         return
             
-    data = response.json()["near_earth_objects"]
+    data = response.json()['near_earth_objects']
 
     table_data = []
-    for object in data[event["date"]]:
+    for object in data[event['date']]:
         table_data.append({
-            "neo_id": object["id"],
-            "name": object["name"],
-            "estimated_diameter_feet_max": round(object["estimated_diameter"]["feet"]["estimated_diameter_max"], 2),
-            "estimated_diameter_feet_min": round(object["estimated_diameter"]["feet"]["estimated_diameter_min"], 2),
-            "relative_velocity_mph":round(float(object["close_approach_data"][0]["relative_velocity"]["miles_per_hour"]), 2),
-            "miss_distance_lunar": round(float(object["close_approach_data"][0]["miss_distance"]["astronomical"]), 2),
-            "miss_distance_astronomical": round(float(object["close_approach_data"][0]["miss_distance"]["astronomical"]), 2)
+            'neo_id': object['id'],
+            'name': object['name'],
+            'estimated_diameter_feet_max': round(object['estimated_diameter']['feet']['estimated_diameter_max'], 2),
+            'estimated_diameter_feet_min': round(object['estimated_diameter']['feet']['estimated_diameter_min'], 2),
+            'relative_velocity_mph':round(float(object['close_approach_data'][0]['relative_velocity']['miles_per_hour']), 2),
+            'miss_distance_lunar': round(float(object['close_approach_data'][0]['miss_distance']['lunar']), 2),
+            'miss_distance_astronomical': round(float(object['close_approach_data'][0]['miss_distance']['astronomical']), 2)
         })
     
 # ---- UPLOAD DATA TO DYNAMODB ---- #
     for date in data.keys():    
         put_operations = [{
-            "PutRequest": {
-                "Item": {
-                    "neo_id": {
-                        "N": object["neo_id"]
+            'PutRequest': {
+                'Item': {
+                    'neo_id': {
+                        'N': object['neo_id']
                     },
-                    "close_approach_date": {
-                        "S": event["date"]
+                    'close_approach_date': {
+                        'S': event['date']
                     },
-                    "name": {
-                        "S": object["name"]
+                    'name': {
+                        'S': object['name']
                     },
-                    "estimated_diameter_feet_max": {
-                        "N": str(object["estimated_diameter_feet_max"])
+                    'estimated_diameter_feet_max': {
+                        'N': str(object['estimated_diameter_feet_max'])
                     },
-                    "estimated_diameter_feet_min": {
-                        "N": str(object["estimated_diameter_feet_min"])
+                    'estimated_diameter_feet_min': {
+                        'N': str(object['estimated_diameter_feet_min'])
                     },
-                    "relative_velocity_mph": {
-                        "N": str(object["relative_velocity_mph"])
+                    'relative_velocity_mph': {
+                        'N': str(object['relative_velocity_mph'])
                     },
-                    "miss_distance_lunar": {
-                        "N": str(object["miss_distance_lunar"])
+                    'miss_distance_lunar': {
+                        'N': str(object['miss_distance_lunar'])
                     },
-                    "miss_distance_astronomical": {
-                        "N": str(object["miss_distance_astronomical"])
+                    'miss_distance_astronomical': {
+                        'N': str(object['miss_distance_astronomical'])
                     },
-                    "uploaded_on": {
-                        "N": str(time.time() + (24 * 60 * 60))
+                    'uploaded_on': {
+                        'N': str(time.time() + (24 * 60 * 60))
                     }
                 }
             }
         } for object in table_data]
         put_data = dynamodb_client.batch_write_item(
-            RequestItems = {os.environ.get("DYNAMO_DB_TABLE_NAME"): put_operations}
+            RequestItems = {os.environ.get('DYNAMO_DB_TABLE_NAME'): put_operations}
         )
         
